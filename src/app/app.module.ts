3dc89a0003a817d1b9258652edeacade42a0fa4a
Fernanda Salazar <fergusalazar@hotmail.com>
2023-06-17 20:49:41 -0600

    desgin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CreditsComponent } from './components/credits/credits.component';
+import { PetsComponent } from './components/pets/pets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent,
+    CreditsComponent,
+    PetsComponent
   ],
   imports: [
     BrowserModule,
